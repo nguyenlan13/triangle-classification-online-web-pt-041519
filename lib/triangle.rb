d12cc1daa171edd0bfe16955297da13e3e7deb7d
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-27 05:45:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,6 @@
 class Triangle
   
+  attr_accessor :side1, :side2, :side3
   
   
   def initialize(side1, side2, side3)
@@ -9,6 +10,8 @@ class Triangle
   end
   
   def kind(type)
+    
+    
     return type
     
     
